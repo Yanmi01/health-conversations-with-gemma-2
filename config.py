1a115e604c2e8d21385c7023ee3bb0e9a58b0e26
Jesuyanmife Egbewale <egbewaleyanmife@gmail.com>
2025-08-20 21:59:15 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+MODEL_NAME = "google/gemma-2b-it"
+NEW_MODEL_REPO = "Yanmife/gemma-2b-health-fp-it"
+WANDB_PROJECT = "Fine-tuning-Gemma-2B-health-fp-it"
+
+MAX_SEQ_LEN = 512
+NUM_EPOCHS = 2
+BATCH_SIZE = 1
+GRAD_ACCUM = 8
+LEARNING_RATE = 1e-5
