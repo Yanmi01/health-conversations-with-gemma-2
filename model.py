1a478b6b3e69e60f125aeba9efb64026616a2174
Jesuyanmife Egbewale <egbewaleyanmife@gmail.com>
2025-08-29 13:01:48 +0100

    Update LoRA configuration and tokenizer settings

--- model.py
+++ model.py
@@ -5,7 +5,6 @@ from config import MODEL_NAME, MAX_SEQ_LEN
 
 def load_tokenizer():
     tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME, trust_remote_code=True)
-    tokenizer.model_max_length = MAX_SEQ_LEN
     return tokenizer
 
 def load_base_model():
@@ -13,6 +12,7 @@ def load_base_model():
         MODEL_NAME,
         torch_dtype=torch.bfloat16 if torch.cuda.is_bf16_supported() else torch.float16,
         device_map="auto",
+        trust_remote_code=False,
         low_cpu_mem_usage=True,
     )
     return model
@@ -20,8 +20,8 @@ def load_base_model():
 def apply_lora(model):
     lora_config = LoraConfig(
         r=64,
-        lora_alpha=32,
-        target_modules=['up_proj','down_proj','gate_proj','k_proj','q_proj','v_proj','o_proj'],
+        lora_alpha=64,
+        target_module="all-linear",
         lora_dropout=0.05,
         bias="none",
         task_type="CAUSAL_LM"
